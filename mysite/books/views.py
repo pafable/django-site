0c27f9a795533fb1660b2922539849ddca0b3716
pafable <philafable@gmail.com>
2018-12-02 23:04:01 -0500

    added detail view

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h>This is the books homepage</h>")
-    
+
+def detail(request,book_id):
+    return HttpResponse("<h2> Details for Book ID:" + str(book_id) + "</h2>")
