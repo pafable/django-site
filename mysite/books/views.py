38acdd63d51521e31e8560a10436ec4f38d741d5
pafable <philafable@gmail.com>
2018-12-03 22:10:15 -0500

    switched to using render

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     all_books = Book.objects.all()
@@ -10,8 +11,7 @@ def index(request):
     context = {
         'all_books': all_books
     }
-
-    return HttpResponse(template.render(context,request))
+    return render(request,'books/index.html',context)
 
 def detail(request,book_id):
     return HttpResponse("<h2> Details for Book ID:" + str(book_id) + "</h2>")
