fade2fda55eed09fb2665a77011875cadae7f77f
pafable <philafable@gmail.com>
2018-12-03 21:55:39 -0500

    created template

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Book
+from django.template import loader
 
 def index(request):
     all_books = Book.objects.all()
-    html = ''
-    for book in all_books:
-        url = '/books/' + str(book.id) + '/'
-        html += '<a href="' + url + '">' + str(book.name) + '</a><br>'
-        
-    return HttpResponse(html)
+    template = loader.get_template('books/index.html')
+    context = {
+        'all_books': all_books
+    }
+
+    return HttpResponse(template.render(context,request))
 
 def detail(request,book_id):
     return HttpResponse("<h2> Details for Book ID:" + str(book_id) + "</h2>")
