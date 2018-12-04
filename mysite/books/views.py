34b8a4ee69236832413930233408d00bfe8e8e39
pafable <philafable@gmail.com>
2018-12-03 21:24:21 -0500

    connected DB to app

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Book
 
 def index(request):
-    return HttpResponse("<h>This is the books homepage</h>")
+    all_books = Book.objects.all()
+    html = ''
+    for book in all_books:
+        url = '/books/' + str(book.id) + '/'
+        html += '<a href="' + url + '">' + str(book.name) + '</a><br>'
+        
+    return HttpResponse(html)
 
 def detail(request,book_id):
     return HttpResponse("<h2> Details for Book ID:" + str(book_id) + "</h2>")
